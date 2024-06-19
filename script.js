87ff951ab15137cede25e7d1480535a93ca645e6
Jorge Ojeda <jdojeda@macProM3.local>
2024-06-19 13:41:13 -0400

    segundo commit

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 function toggleMode() {
   const html = document.documentElement
 
-  // if(html.classList.contains('light')) {
-  //   html.classList.remove('light')
-  // } else {
-  //   html.classList.add('light')
-  // }
-
   html.classList.toggle('light')
-
-
-  // tomar el tag <img>
-  // sustituir la imagen actual, según la siguiente condición:
-  // 1. Si es light, cargar la imagen "image-light", sino mantener el original.
 
   const img = document.querySelector("#profile img")
 
